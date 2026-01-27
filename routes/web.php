b260ee32f8ac7314f14eefd2cfaf2347ff8e6f60
Abdo reda <abdoredapro40@gmail.com>
2026-01-27 18:23:34 +0200

    Add second about page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Controllers\HomeController;
 
 $router->get('home', [HomeController::class, 'index']);
 $router->get('about', [HomeController::class, 'about']);
+$router->get('about', [HomeController::class, 'about']);
 
 
 
