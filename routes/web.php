6bdb8858222c85f4ad38bffe966ab45be71ccfa4
Abdulrhman Reda <abdoredapro40@gmail.com>
2026-01-28 19:31:41 +0200

    Add PDF Feature

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Controllers\HomeController;
 $router->get('home', [HomeController::class, 'index']);
 $router->get('about', [HomeController::class, 'about']);
 $router->get('about', [HomeController::class, 'about']);
+$router->get('pdf', [HomeController::class, 'pdf']);
 
 
 
